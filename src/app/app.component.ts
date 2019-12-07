d4aaa11541629a09fee5665607fc9f3998c27f1b
Michaella Babbitt <tinydynamine@gmail.com>
2019-12-07 11:56:48 -0600

    second commit- added satellite components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: ['./app.component.css'],
+  sourceList: Satellite[];
+  })
+
+
 export class AppComponent {
   title = 'orbit-report';
 }
